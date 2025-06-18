19a57038f8dff0167f6dcaef59d87e4d24826061
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-06-18 10:28:18 +0200

    Update detection.py

--- utils/detection.py
+++ utils/detection.py
@@ -16,10 +16,13 @@ def detect_table_cells(preprocessed_img):
     contours, _ = cv2.findContours(table_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     boxes = []
-    for cnt in contours:
-        x, y, w, h = cv2.boundingRect(cnt)
-        if w > 40 and h > 20:
-            boxes.append((x, y, w, h))
-            cv2.rectangle(img_draw, (x, y), (x + w, y + h), (0, 255, 0), 2)
+   for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    area = w * h
+    if w > 40 and h > 20 and area > 1500:
+        # Check if box is below the main table (likely a total row)
+        if y > preprocessed_img.shape[0] * 0.85:
+            h += 20  # artificially expand totals row height
+        boxes.append((x, y, w, h))
 
     return img_draw, sorted(boxes, key=lambda b: (b[1], b[0]))
