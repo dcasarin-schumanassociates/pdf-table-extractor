f0079d87ceb36e568dc21bf6e7326619efc31aa4
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-06-18 11:29:05 +0200

    Update text_extract.py

--- utils/text_extract.py
+++ utils/text_extract.py
@@ -1,12 +1,9 @@
 import pdfplumber
 
-def try_pdf_text_extraction(file_bytes):
+def try_pdf_text_extraction(file_like_object):
     tables_by_page = []
-
-    with pdfplumber.open(file_bytes) as pdf:
+    with pdfplumber.open(file_like_object) as pdf:
         for page in pdf.pages:
-            # Try table extraction (uses underlying layout)
             tables = page.extract_tables()
             tables_by_page.append(tables)
-
     return tables_by_page
