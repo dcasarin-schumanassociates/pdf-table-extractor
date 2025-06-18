c5109660870eb37f36b05a08b302ecd2fdb72526
dcasarin-schumanassociates <davide.casarin@schumanassociates.com>
2025-06-18 11:55:22 +0200

    Update text_extract.py

--- utils/text_extract.py
+++ utils/text_extract.py
@@ -4,6 +4,6 @@ def try_pdf_text_extraction(file_like_object):
     tables_by_page = []
     with pdfplumber.open(file_like_object) as pdf:
         for page in pdf.pages:
-            tables = page.extract_tables()
+            tables = page.extract_tables(table_settings={"vertical_strategy": "lines", "horizontal_strategy": "lines"})
             tables_by_page.append(tables)
     return tables_by_page
